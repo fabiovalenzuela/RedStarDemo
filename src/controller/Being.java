03046b07b7d80ee2b00500c6dd8cff0302439823
fabiovalenzuela <fvalenzuela1@ggc.edu>
2020-10-27 15:53:44 -0400

    Damage system suggestion setup for affecting both the player and the enemy ** NEED MORE SUGGESTIONS FROM GROUP

--- src/controller/Being.java
+++ src/controller/Being.java
@@ -186,6 +186,21 @@ abstract public class Being {
      * Purpose: Returns a String of the Monster class
      * @return
      */
+
+    /*
+     * Method: Damage
+     * Purpose: Damages both the player and the monster using a random value within a range
+     * @return
+     */
+    public int Damage(){
+        int damageDealt = random.randint(20,30);
+        int damageRecieved = random.randint(0,damageDealt);
+        enemyHealth -= damageDealt;
+        playerHealth -= damageRecieved;
+
+    }
+
+
     @Override
     public String toString() {
         return "Monster iD = " + iD +
